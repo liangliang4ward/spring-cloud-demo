61d03e71a67f07024031dd0a7e5eb734463a4000
gll <gaoliangliang@zailingtech.com>
2019-05-30 20:05:33 +0800

    '代码提交处理'

---
+++ lift-agent/src/main/java/com/xzl/agent/api/PingRemoteApi.java
@@ -0,0 +1,13 @@
+package com.xzl.agent.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @author gll
+ * 2019/5/30 20:03
+ */
+@FeignClient
+public interface PingRemoteApi {
+
+    public
+}
