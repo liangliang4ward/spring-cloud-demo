e56301feabd522f6d1ae864631cbb12b5987367c
gll <gaoliangliang@zailingtech.com>
2019-06-05 16:37:56 +0800

    修改配置处理。

--- demo-template/src/test/java/com/xzl/generaotr/MysqlGenerator.java
+++ demo-template/src/test/java/com/xzl/generaotr/MysqlGenerator.java
@@ -85,7 +85,7 @@ public class MysqlGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输入文件名称
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                return projectPath + "/src/main/resources/mapper/"
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
